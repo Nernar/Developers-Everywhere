39264b4dbb4f6fcd9b1477807cb863287d4f1c28
MaXFeeD <46795182+MaXFeeD@users.noreply.github.com>
2024-07-13 00:41:28 +0300

    Refactoring obfuscated code
    - New modders photos
    - Client only, can join servers
    - TGA textures support
    - Pack loading redesign
    - Improved chances of more rare developers appearing
    - Balanced number of random textures
    - Graphics backup is created correctly
    - Starting game returns backup as main one
    - Balanced chances of certain splashes appearing
    - Added a new pack icon
    - First launch correctly creates a resource pack
    - Improved methods for determining random photo part for textures
    - Atlas cleanup option cleans all textures and data
    - Textures and data that cannot be processed logging

--- launcher.js
+++ launcher.js
@@ -5,3 +5,11 @@ try {
 } catch (e) {}
 
 Launch();
+
+(function() {
+	let graphicsOutput = new java.io.File(__packdir__ + ".cached_graphics");
+	let graphicsBackup = new java.io.File(__packdir__ + ".cached_graphics.bak");
+	if (graphicsOutput.exists() && graphicsBackup.exists()) {
+		com.zhekasmirnov.innercore.utils.FileTools.copy(graphicsBackup, graphicsOutput);
+	}
+})();
